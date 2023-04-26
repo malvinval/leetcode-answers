eacb00f4acb7e23bc051596d08892d5e12243e0b
Malvin Valerian <malvinval09@gmail.com>
2023-04-26 11:03:25 +0700

    37 problems solved

---
+++ easy/37-power-of-two/app.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+/**
+ * Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+    An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+    Example 1:
+
+    Input: n = 1
+    Output: true
+    Explanation: 20 = 1
+    Example 2:
+
+    Input: n = 16
+    Output: true
+    Explanation: 24 = 16
+    Example 3:
+
+    Input: n = 3
+    Output: false
+*/
+
+bool isPowerOfTwo(int n) {
+    double dn = (double) n;
+    
+    while(dn >= 2.0) dn /= 2;
+
+    return dn == 1.0;
+}
+
+int main() {
+    std::cout << isPowerOfTwo(32) << std::endl;
+
+    return EXIT_SUCCESS;
+}
