986d2ee65eaae0b3433974ec381fa6ed200380f2
Malvin Valerian <malvinval09@gmail.com>
2023-04-29 11:58:22 +0700

    47 problems solved

--- easy/47-reverse-linked-list/app.cpp
+++ easy/47-reverse-linked-list/app.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * 
+ * Example:
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+*/
+
 struct ListNode {
     int val;
     ListNode *next;
